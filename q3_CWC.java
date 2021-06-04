20e0289bc21dabfffbd13bcd5758c52751886b88
SoumitraPanda <85274921+SoumitraPanda@users.noreply.github.com>
2021-06-04 23:59:30 +0530

    Update q3_CWC.java

--- q3_CWC.java
+++ q3_CWC.java
@@ -2,32 +2,24 @@
 import java.util.Scanner;
 public class q3_CWC {
 
-	
-	    public static void main(String args[])
-	    {
-	        Scanner sc = new Scanner(System.in);
-	        System.out.print("Enter number till u want to print : ");
-	        int n=sc.nextInt();
 
-	        int a = 0,b = 1,c = 1;
-	        int d = a+b+c;
-
-	   
-	        System.out.println (a+" ");
-	        System.out.println (b+" ");
-	        System.out.println (c+" ");
-	        System.out.println (d+" ");
-	        
-	        for(int i=5;i<=n;i++)
-	        {
-	            
-	            a=b;
-	            b=c;
-	            c=d;
-	            d=a+b+c;
-	       
-
-	        System.out.println(d+" ");
-	    }
-	}
-}	    
+    public static void main(String [] args)
+    { 
+        Scanner in=new Scanner(System.in);
+        System.out.println("enter n");
+        int x=in.nextInt();
+        int a=0,b=1,c=1;
+        System.out.print(a+" ");
+        if(x>1)
+            System.out.print(b+" ");
+        if(x>2)
+            System.out.print(c+" ");
+                for(int i=3;i<n;i++)
+                {
+                    int y=a+b+c;
+                    a=b;
+                    c=y;
+                    System.out.print(y+" ");
+                }
+    }
+}    
