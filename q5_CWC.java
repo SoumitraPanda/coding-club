bbef3e5c28eaeb1f4d194e7cd75de9e12688d143
SoumitraPanda <85274921+SoumitraPanda@users.noreply.github.com>
2021-06-06 00:02:03 +0530

    Update q5_CWC.java

--- q5_CWC.java
+++ q5_CWC.java
@@ -3,14 +3,16 @@ public class q5_CWC {
 	
 	public static void main(String[] args) {
 		
-     int[] arr = {9,1,7,2,8};
-     int count =0;
+     int[] arr = {8,6,4,9,1};
      for(int i=0;i<arr.length-1;i++) {
     	 for(int j=i+1;j<arr.length;j++) {
     		 if(arr[i]>arr[j]) {
     			 System.out.println("{"+arr[i]+" , "+arr[j]+"}");
     			 count++;
     		 }
+		 else{
+			  System.out.println(0);
+		 }
     	 }
     	 
      }
